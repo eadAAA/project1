259b67fe4a56f2b13b59e32b0dac0b82d36c39b9
eadAAA <91408907+eadAAA@users.noreply.github.com>
2021-11-01 17:53:25 +0600

    Delete bookCatalog directory

--- bookCatalog/bookCatalog/src/main/java/kz/bcc/bookCatalog/service/BookInformationService.java
+++
@@ -1,8 +0,0 @@
-package kz.bcc.bookCatalog.service;
-
-import kz.bcc.bookCatalog.model.Book;
-
-public interface BookInformationService {
-
-    Book getBookInformationById(Long id);
-}
--- bookCatalog/bookCatalog/src/main/java/kz/bcc/bookCatalog/service/BookRatingService.java
+++
@@ -1,6 +0,0 @@
-package kz.bcc.bookCatalog.service;
-
-public interface BookRatingService {
-
-    Double getBookRatingById(Long id);
-}
