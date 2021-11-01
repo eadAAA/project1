cfb3f6dc9a2022d403943bafb06816258b254bd0
eadAAA <91408907+eadAAA@users.noreply.github.com>
2021-11-01 17:53:45 +0600

    Delete books directory

--- books/src/main/java/com/example/demo/service/BookService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.BookDTO;
-
-public interface BookService {
-    void addBook(int isbn, String name, String description);
-    BookDTO getBook(int isbn);
-}
